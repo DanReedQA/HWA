2c96ef467c336006ee9e1046f0ca24f2ecdf40ee
DanReedQA <dan@reed-wayside.co.uk>
2020-05-05 15:31:36 +0100

    added values to CardDTO & BoxDTO

--- HWA/src/main/java/com/qa/dto/BoxDTO.java
+++ HWA/src/main/java/com/qa/dto/BoxDTO.java
@@ -1,4 +1,48 @@
 package com.qa.dto;
 
+import com.qa.domain.Card;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BoxDTO {
+
+    private Long boxId;
+    //  private Long cardId;  < --  To be added later
+    private String boxName;
+//  private Long totalValue;  < --  To be added later
+//  private Long totalStock;  < --  To be added later
+    private List<CardDTO> cards;
+
+    public BoxDTO() {
+    }
+
+    public BoxDTO(String boxName, List<CardDTO> cards) {
+        super();
+        this.boxName = boxName;
+        this.cards = cards;
+    }
+
+    public Long getBoxId() {
+        return boxId;
+    }
+
+    public void setBoxId(Long boxId) {
+        this.boxId = boxId;
+    }
+
+    public String getBoxName() {
+        return boxName;
+    }
+
+    public void setBoxName(String boxName) {
+        this.boxName = boxName;
+    }
+
+    public List<CardDTO> getCards() {
+        return cards;
+    }
+
+    public void setCards(List<CardDTO> cards) {
+        this.cards = cards;
+    }
 }
--- HWA/src/main/java/com/qa/dto/CardDTO.java
+++ HWA/src/main/java/com/qa/dto/CardDTO.java
@@ -1,4 +1,59 @@
 package com.qa.dto;
 
 public class CardDTO {
+    private Long cardId;
+    private String cardName;
+    private String rarity;
+    private Long stock;
+    private Long value;
+
+    public CardDTO() {
+    }
+
+    public CardDTO(String cardName, String rarity, Long stock, Long value){
+        this.cardName = cardName;
+        this.rarity = rarity;
+        this.stock = stock;
+        this.value = value;
+    }
+
+    public Long getCardId() {
+        return cardId;
+    }
+
+    public void setCardId(Long cardId) {
+        this.cardId = cardId;
+    }
+
+    public String getCardName() {
+        return cardName;
+    }
+
+    public void setCardName(String cardName) {
+        this.cardName = cardName;
+    }
+
+    public String getRarity() {
+        return rarity;
+    }
+
+    public void setRarity(String rarity) {
+        this.rarity = rarity;
+    }
+
+    public Long getStock() {
+        return stock;
+    }
+
+    public void setStock(Long stock) {
+        this.stock = stock;
+    }
+
+    public Long getValue() {
+        return value;
+    }
+
+    public void setValue(Long value) {
+        this.value = value;
+    }
 }
