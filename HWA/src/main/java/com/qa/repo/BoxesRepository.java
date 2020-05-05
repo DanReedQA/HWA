113fda3a7cc8e76cfd507dc3da2f29ae7d107675
DanReedQA <dan@reed-wayside.co.uk>
2020-05-05 10:40:29 +0100

    added BoxesRepository

--- HWA/src/main/java/com/qa/repo/BoxesRepository.java
+++ HWA/src/main/java/com/qa/repo/BoxesRepository.java
@@ -1,4 +1,12 @@
 package com.qa.repo;
 
-public interface BoxesRepository {
+import com.qa.domain.Box;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BoxesRepository extends JpaRepository<Box, Long> {
+
+    Box findByBoxName(String boxName);
+
 }
