3e6b4d025daa2b1d76fe1ffcee25744a2c20e10e
DanReedQA <dan@reed-wayside.co.uk>
2020-05-05 10:54:06 +0100

    added functionality for BoxController & BoxService

--- HWA/src/main/java/com/qa/rest/BoxController.java
+++ HWA/src/main/java/com/qa/rest/BoxController.java
@@ -1,4 +1,47 @@
 package com.qa.rest;
 
+import com.qa.domain.Box;
+import com.qa.service.BoxService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
+import java.util.List;
+
+@RestController
 public class BoxController {
+
+    private final BoxService service;
+
+    @Autowired
+    public BoxController(BoxService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/getAllBoxes")
+    public List<Box> getAllBoxes(){
+        return this.service.readBoxes();
+    }
+
+    @PostMapping("/createBox")
+    public Box createBox(@RequestBody Box box){
+        return this.service.createBox(box);
+    }
+
+    @DeleteMapping("/deleteBox/{boxId}")
+    public boolean deleteBox(@PathVariable Long boxId){
+        return this.service.deleteBox(boxId);
+    }
+
+    @GetMapping("/getBoxById/{boxId}")
+    public Box getBoxById(@PathVariable Long boxId){
+        return this.service.findBoxById(boxId);
+    }
+
+    @PutMapping("/updateBox/{boxId}")
+    public Box updateBox(@PathVariable Long boxId, @RequestBody Box box){ return this.service.updateBox(boxId, box); }
+
+    @PutMapping("/updateBox2")
+    public Box updateCard2(@PathParam("boxId") Long boxId, @RequestBody Box box){ return this.service.updateBox(boxId, box); }
+
 }
--- HWA/src/main/java/com/qa/service/BoxService.java
+++ HWA/src/main/java/com/qa/service/BoxService.java
@@ -1,4 +1,48 @@
 package com.qa.service;
 
+import com.qa.domain.Box;
+import com.qa.exceptions.BoxNotFoundException;
+import com.qa.repo.BoxesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class BoxService {
+
+    private final BoxesRepository repo;
+
+    @Autowired
+    public BoxService(BoxesRepository repo) {
+        this.repo = repo;
+    }
+
+    public List<Box> readBoxes(){
+        return this.repo.findAll();
+    }
+
+    public Box createBox(Box box){
+        return this.repo.save(box);
+    }
+
+    public Box findBoxById(Long boxId){
+        return this.repo.findById(boxId).orElseThrow(BoxNotFoundException::new);
+    }
+
+    public Box updateBox(Long boxId, Box box){
+        Box update = findBoxById(boxId);
+        update.setBoxName(box.getBoxName());
+        return this.repo.save(update);
+    }
+
+    public boolean deleteBox(Long boxId){
+        if(!this.repo.existsById(boxId)){
+            throw new BoxNotFoundException();
+        }
+        this.repo.deleteById(boxId);
+        return this.repo.existsById(boxId);
+    }
+
+
 }
