9a377af6421f83d8fbb000b3328ffe18089bd180
DanReedQA <dan@reed-wayside.co.uk>
2020-05-06 10:30:18 +0100

    added Customer variable values & getters and setters
    added CustomerNotFoundException functionality

--- HWA/src/main/java/com/qa/domain/Customer.java
+++ HWA/src/main/java/com/qa/domain/Customer.java
@@ -1,4 +1,56 @@
 package com.qa.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long customerId;
+    private String firstName;
+    private String surname;
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Customer)) return false;
+        Customer customer = (Customer) o;
+        return Objects.equals(getCustomerId(), customer.getCustomerId()) &&
+                Objects.equals(getFirstName(), customer.getFirstName()) &&
+                Objects.equals(getSurname(), customer.getSurname());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCustomerId(), getFirstName(), getSurname());
+    }
 }
