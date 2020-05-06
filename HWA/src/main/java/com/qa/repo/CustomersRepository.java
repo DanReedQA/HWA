4a882d467a6bc388f1b2a04fc7b2aa2f107e3751
DanReedQA <dan@reed-wayside.co.uk>
2020-05-06 10:35:58 +0100

    added CustomersRepository functionality

--- HWA/src/main/java/com/qa/repo/CustomersRepository.java
+++ HWA/src/main/java/com/qa/repo/CustomersRepository.java
@@ -1,4 +1,14 @@
 package com.qa.repo;
 
-public interface CustomersRepository {
+import com.qa.domain.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomersRepository extends JpaRepository<Customer, Long> {
+
+    Customer findByCustomerFirstName(String firstName);
+
 }
