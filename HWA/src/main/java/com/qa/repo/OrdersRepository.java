76d056311954caece764e079c204fc250a4e6160
DanReedQA <dan@reed-wayside.co.uk>
2020-05-06 11:59:26 +0100

    added OrderRepository functionality

--- HWA/src/main/java/com/qa/repo/OrdersRepository.java
+++ HWA/src/main/java/com/qa/repo/OrdersRepository.java
@@ -1,4 +1,14 @@
 package com.qa.repo;
 
-public interface OrdersRepository {
+import com.qa.domain.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface OrdersRepository extends JpaRepository<Order, Long> {
+
+    Order findByOrderId(Long orderId);
+
 }
