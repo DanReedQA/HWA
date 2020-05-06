a6fd4200c11cc88c47c30b5aab47fc739e4edf63
DanReedQA <dan@reed-wayside.co.uk>
2020-05-06 10:33:16 +0100

    added CustomerDTO variables

--- HWA/src/main/java/com/qa/dto/CustomerDTO.java
+++ HWA/src/main/java/com/qa/dto/CustomerDTO.java
@@ -1,4 +1,41 @@
 package com.qa.dto;
 
 public class CustomerDTO {
+
+    private Long customerId;
+    private String firstName;
+    private String surname;
+
+    public CustomerDTO() {
+
+    }
+
+    public CustomerDTO(String firstName, String surname) {
+        this.firstName = firstName;
+        this.surname = surname;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
 }
