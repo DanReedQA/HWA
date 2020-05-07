fc8c7065cd0cbbd9821c1b5d110692dc636681e7
DanReedQA <dan@reed-wayside.co.uk>
2020-05-07 15:11:47 +0100

    added Order class getters & setters

--- HWA/src/main/java/com/qa/domain/Order.java
+++ HWA/src/main/java/com/qa/domain/Order.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Objects;
 
 @Entity
 public class Order {
@@ -11,8 +12,65 @@ public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long orderId;
-//  private Long customerId; <-- Add later
-//  private Long cardID; <-- Add later
-//  private Long orderValue; <-- Add later
+    private Long customerId;
+    private Long cardId;
+    private Long orderValue;
 
+    public Order() {
+
+    }
+
+    public Order(Long customerId, Long cardId, Long orderValue) {
+        this.customerId = customerId;
+        this.cardId = cardId;
+        this.orderValue = orderValue;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public Long getCardId() {
+        return cardId;
+    }
+
+    public void setCardId(Long cardId) {
+        this.cardId = cardId;
+    }
+
+    public Long getOrderValue() {
+        return orderValue;
+    }
+
+    public void setOrderValue(Long orderValue) {
+        this.orderValue = orderValue;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Order)) return false;
+        Order order = (Order) o;
+        return Objects.equals(getOrderId(), order.getOrderId()) &&
+                Objects.equals(getCustomerId(), order.getCustomerId()) &&
+                Objects.equals(getCardId(), order.getCardId()) &&
+                Objects.equals(getOrderValue(), order.getOrderValue());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getOrderId(), getCustomerId(), getCardId(), getOrderValue());
+    }
 }
