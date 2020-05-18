2202320573ee31476d661c6980ad99ae2203248e
DanReedQA <dan@reed-wayside.co.uk>
2020-05-18 15:05:31 +0100

    first attempt at webpage

--- HWA/src/main/resources/static/html/js/index.js
+++ HWA/src/main/resources/static/html/js/index.js
@@ -18,3 +18,12 @@ function openBox(evt, boxName) {
     document.getElementById(boxName).style.display = "block";
     evt.currentTarget.className += " active";
 }
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
